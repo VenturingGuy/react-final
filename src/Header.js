94e55249b0493fbd1338da60083cbf55636fe1bd
Luis <LVentura335@Gmail.com>
2020-12-10 14:54:13 -0800

    first commit

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+function Header() {
+    return (
+        <div className="Heading">
+            <h1 id="landing">The Boar Zone</h1>
+            <h2 id="content">Content</h2>
+            <h2 id="about-me">About Me</h2>
+        </div>
+    )
+}
+
+export default Header
